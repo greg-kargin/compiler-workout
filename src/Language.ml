6eb382eb2760954fc5b83d3cb9960ed978001806
Grigorii Kargin <greg.kargin@gmail.com>
2018-03-06 12:27:11 +0300

    hw3

--- src/Language.ml
+++ src/Language.ml
@@ -5,13 +5,13 @@ open GT
 
 (* Opening a library for combinator-based syntax analysis *)
 open Ostap.Combinators
-       
+
 (* Simple expressions: syntax and semantics *)
 module Expr =
   struct
-    
-    (* The type for expressions. Note, in regular OCaml there is no "@type..." 
-       notation, it came from GT. 
+
+    (* The type for expressions. Note, in regular OCaml there is no "@type..."
+       notation, it came from GT.
     *)
     @type t =
     (* integer constant *) | Const of int
@@ -25,14 +25,14 @@ module Expr =
         +, -                 --- addition, subtraction
         *, /, %              --- multiplication, division, reminder
     *)
-                                                            
+
     (* State: a partial map from variables to integer values. *)
-    type state = string -> int 
+    type state = string -> int
 
     (* Empty state: maps every variable into nothing. *)
     let empty = fun x -> failwith (Printf.sprintf "Undefined variable %s" x)
 
-    (* Update: non-destructively "modifies" the state s by binding the variable x 
+    (* Update: non-destructively "modifies" the state s by binding the variable x
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
@@ -40,24 +40,75 @@ module Expr =
     (* Expression evaluator
 
           val eval : state -> t -> int
- 
-       Takes a state and an expression, and returns the value of the expression in 
+
+       Takes a state and an expression, and returns the value of the expression in
        the given state.
     *)
-    let eval _ = failwith "Not implemented yet"
+
+    let lt x y = if x < y then 1 else 0
+    let gt x y = if x > y then 1 else 0
+    let nt x   = if x == 0 then 1 else 0
+    let le x y = nt @@ gt x y
+    let ge x y = nt @@ lt x y
+    let eq x y = if x == y then 1 else 0
+    let neq x y = nt @@ eq x y
+    let con x y = if x == 0 || y == 0 then 0 else 1
+    let dis x y = nt @@ con (nt x) (nt y)
+
+    let eval_binop op =
+      match op with
+        "+"  -> ( + )
+      | "-"  -> ( - )
+      | "*"  -> ( * )
+      | "/"  -> ( / )
+      | "%"  -> ( mod )
+      | "<"  -> lt
+      | ">"  -> gt
+      | "<=" -> le
+      | ">=" -> ge
+      | "==" -> eq
+      | "!=" -> neq
+      | "&&" -> con
+      | "!!" -> dis
+      | _    -> failwith "unknown binop"
+
+    let rec eval s e =
+      match e with
+        Const n            -> n
+      | Var   v            -> s v
+      | Binop (op, e1, e2) -> (eval_binop op) (eval s e1) (eval s e2)
 
     (* Expression parser. You can use the following terminals:
 
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string
-   
+
     *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      parse: expr;
+      expr:
+        !(Ostap.Util.expr
+            (fun x -> x)
+            [|
+              `Lefta , [ostap ("!!"), (fun x y -> Binop ("!!", x, y));
+                        ostap ("&&"), (fun x y -> Binop ("&&", x, y))];
+              `Nona ,  [ostap (">"), (fun x y -> Binop (">", x, y));
+                        ostap (">="), (fun x y -> Binop (">=", x, y));
+                        ostap ("<"), (fun x y -> Binop ("<", x, y));
+                        ostap ("<="), (fun x y -> Binop ("<=", x, y))];
+              `Lefta , [ostap ("+"), (fun x y -> Binop ("+", x, y));
+                        ostap ("-"), (fun x y -> Binop ("-", x, y))];
+              `Lefta , [ostap ("*"), (fun x y -> Binop ("*", x, y));
+                        ostap ("/"), (fun x y -> Binop ("/", x, y));
+                        ostap ("%"), (fun x y -> Binop ("%", x, y))];
+            |]
+            primary
+        );
+      primary: x:IDENT {Var x}| n:DECIMAL {Const n} | -"(" expr -")"
     )
 
   end
-                    
+
 (* Simple statements: syntax and sematics *)
 module Stmt =
   struct
@@ -70,7 +121,7 @@ module Stmt =
     (* composition                      *) | Seq    of t * t with show
 
     (* The type of configuration: a state, an input stream, an output stream *)
-    type config = Expr.state * int list * int list 
+    type config = Expr.state * int list * int list
 
     (* Statement evaluator
 
@@ -78,19 +129,39 @@ module Stmt =
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let eval _ = failwith "Not implemented yet"
+
+    let rec eval c st =
+      let (s, i, o) = c in
+      match st with
+        Read x  -> let i_head = List.hd i in
+                   let i_tail = List.tl i in
+                   let s' = (Expr.update x i_head s) in
+                   (s', i_tail, o)
+      | Write e -> let r = Expr.eval s e in
+                   let o' = (o @ [r]) in
+                   (s, i, o')
+      | Assign (x, e) -> let r = Expr.eval s e in
+                         let s' = (Expr.update x r s) in
+                         (s', i, o)
+      | Seq (st1, st2) -> let c' = eval c st1 in
+                          eval c' st2
 
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented yet"}
+      parse  : stmt | seq;
+      stmt   : read | write | assign;
+      read   : -"read" -"(" var:IDENT -")" {Read var};
+      write  : -"write" -"(" expr:!(Expr.parse) -")" {Write expr};
+      assign : var:IDENT -":=" expr:!(Expr.parse) {Assign (var, expr)};
+      seq    : lval:stmt ";" rval:stmt {Seq (lval, rval)}
     )
-      
+
   end
 
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
-type t = Stmt.t    
+type t = Stmt.t
 
 (* Top-level evaluator
 
--- src/SM.ml
+++ src/SM.ml
@@ -1,16 +1,16 @@
-open GT       
+open GT
 open Language
-       
+
 (* The type for the stack machine instructions *)
 @type insn =
 (* binary operator                 *) | BINOP of string
-(* put a constant on the stack     *) | CONST of int                 
+(* put a constant on the stack     *) | CONST of int
 (* read to stack                   *) | READ
 (* write from stack                *) | WRITE
 (* load a variable to the stack    *) | LD    of string
 (* store a variable from the stack *) | ST    of string with show
 
-(* The type for the stack machine program *)                                                               
+(* The type for the stack machine program *)
 type prg = insn list
 
 (* The type for the stack machine configuration: a stack and a configuration from statement
@@ -23,8 +23,37 @@ type config = int list * Stmt.config
      val eval : config -> prg -> config
 
    Takes a configuration and a program, and returns a configuration as a result
- *)                         
-let eval _ = failwith "Not yet implemented"
+ *)
+
+let exec_insn c p =
+  let (st, sc) = c in
+  let (s, i, o) = sc in
+  match p with
+    BINOP op -> (match st with
+                   s1 :: s2 :: st' -> let r = (Language.Expr.eval_binop op) s1 s2 in
+                                      (r :: st, sc)
+                 | _ -> failwith "not enough elements on stack")
+  | CONST z -> (z :: st, sc)
+  | READ -> let i_hd = List.hd i in
+            let i_tl = List.tl i in
+            (i_hd :: st, (s, i_tl, o))
+  | WRITE -> let st_hd = List.hd st in
+             let st_tl = List.tl st in
+             (st_tl, (s, i, o @ [st_hd]))
+  | LD x -> let v = s x in
+            (v :: st, sc)
+  | ST x -> let st_hd = List.hd st in
+            let st_tl = List.tl st in
+            (st_tl, ((Language.Expr.update x st_hd s), i, o))
+
+
+(* let eval c p = List.fold_left p c exec_insn *)
+
+let rec eval c p =
+  match p with
+    ph :: p' -> eval (exec_insn c ph) p'
+  | [] -> c
+
 
 (* Top-level evaluation
 
@@ -41,4 +70,16 @@ let run p i = let (_, (_, _, o)) = eval ([], (Language.Expr.empty, i, [])) p in 
    Takes a program in the source language and returns an equivalent program for the
    stack machine
  *)
-let compile _ = failwith "Not yet implemented"
+
+let rec comp_e e =
+  match e with
+    Language.Expr.Const v -> [CONST v]
+  | Language.Expr.Var x -> [LD x]
+  | Language.Expr.Binop (op, e1, e2) -> (comp_e e1) @ (comp_e e2) @ [BINOP op]
+
+let rec compile stmt =
+  match stmt with
+    Language.Stmt.Read x -> [READ; ST x]
+  | Language.Stmt.Write e -> (comp_e e) @ [WRITE]
+  | Language.Stmt.Assign (x, e) -> (comp_e e) @ [ST x]
+  | Language.Stmt.Seq (st1, st2) -> (compile st1) @ (compile st2)
